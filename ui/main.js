3a26d126b89552c982e4ffd2276e951e7fa8556a
Ranjan Rabbit <ranjanrab@gmail.com>
2017-02-25 12:02:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,11 @@ console.log('Loaded!');
 //change txt
 var element = document.getElementById("main");
 element.innerHTML="hi";
+
+
+//move img
+
+var img=document.getElementById("madi");
+img.onclick=function(){
+    img.style.marginleft("300px")
+}
